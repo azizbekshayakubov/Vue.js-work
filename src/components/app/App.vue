b29ddae1a6592a7caac76f5036e0bbbe8dcf7821
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2025-12-15 17:41:45 +0500

    added app filter and search-panel 1.30 -minuts

--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -1,16 +1,44 @@
 <template>
-  <div>Hello word</div>
+  <div class="app">
+    <div class="content">
    <AppInfo/>
+   <div class="search-panel">
+    <SearchPanel/>
+   </div>
+   </div>
+   </div>
 </template>
 
 
 <script>
 import AppInfo from "@/components/app-info/AppInfo.vue"
+import SearchPanel from "../search-panel/SearchPanel.vue";
 export default {
     components: {
         AppInfo,
+        SearchPanel,
     }
 }
 </script>
 
-<style></style>
+<style>
+    .app {
+      height: 100vh;
+      color: #000;
+    }
+
+    .content{
+        width: 1000px;
+        min-height: 700px;
+        background-color: #fff;
+        margin: 0 auto;
+        padding: 5rem 0;
+    }
+    .search-panel{
+        margin-top: 2rem;
+        padding: 1.5rem;
+        background-color: #fcfaf5;
+        border-radius: 4px;
+        box-shadow: 15px 15px 15px rgb(0, 0, 0, 0.15);
+    }
+</style>
