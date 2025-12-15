e4f5ff781407afa72aae8802c4668d5c5d916666
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2025-12-15 17:18:17 +0500

    init

---
+++ src/components/app-info/AppInfo.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>APP Info</h1>
+</template>
+
+<script>
+
+ 
+</script>
+
+<style></style>
---
+++ src/components/app/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Hello word</div>
+   <AppInfo/>
+</template>
+
+
+<script>
+import AppInfo from "@/components/app-info/AppInfo.vue"
+export default {
+    components: {
+        AppInfo,
+    }
+}
+</script>
+
+<style></style>
