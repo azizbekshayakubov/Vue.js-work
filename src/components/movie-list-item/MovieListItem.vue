9fb9eeb3f667dd6fe295943e3eaa6200cb9c380e
azizbekshayakubov <115446783+azizbekshayakubov@users.noreply.github.com>
2025-12-24 22:26:16 +0500

    added 1.50 minuts

--- src/components/movie-list-item/MovieListItem.vue
+++ src/components/movie-list-item/MovieListItem.vue
@@ -1,5 +1,13 @@
 <template>
-  <li>Empire of osman</li>
+  <li class="list-group-item d-flex justify-content-between">
+    <span class="list-group-item-label">Omor</span>
+    <input type="number" class="list-group-item-input" defaultValue="811" />
+    <div class="d-flex justify-content-center align-items-center">
+      <button type="button" class="btn-cookie btn-sm">
+        <i></i>
+      </button>
+    </div>
+  </li>
 </template>
 <script>
   export default {};
